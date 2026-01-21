f1d0441b74df55723a754770ea8f82447257ce82
Mykhailo Hakman <deorfeal.it@gmail.com>
2026-01-21 14:55:45 +0100

    feat: add pages/EntranceView, working log-reg

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+const { userId, initAuth } = useUser();
+
+initAuth();
+</script>
+
 <template>
+  <div>
+    <div v-if="!userId">Загрузка</div>
+    <div v-else>Есть</div>
+    <div v-if="!userId">Не залогинен такой</div>
+  </div>
+  <NuxtPage />
+</template>
+
+<!-- <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
-</template>
+</template> -->
